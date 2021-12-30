d69a20e920231faef17c73bddb2254c013de7586
SangYong-Jeong <mmaf@naver.com>
2021-12-30 17:45:36 +0900

    removeTodo 구현부터 시작

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,14 +1,13 @@
 <template>
-  <!-- html -->
-  <div>header</div>
+  <header>
+    <h1>Todo it!</h1>
+  </header>
 </template>
 
-<script>
-export default {
-  // js script
-};
-</script>
-
-<style>
-/* css */
+<style scoped>
+h1 {
+  color: #2f3b52;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
+}
 </style>
