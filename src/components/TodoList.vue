0edaa25f0a8b639740e3417304d500b4f9575ba6
SangYong-Jeong <mmaf@naver.com>
2022-01-01 19:25:45 +0900

    es5 > es6

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <ul>
+    <transition-group name="list" tag="ul">
       <li
         class="shadow"
         v-for="(todoItem, index) in propsdata"
@@ -18,7 +18,7 @@
           <i class="fas fa-trash-alt"></i>
         </span>
       </li>
-    </ul>
+    </transition-group>
   </div>
 </template>
 
@@ -68,5 +68,16 @@ li {
 .textCompleted {
   text-decoration: line-through;
   color: #b3adad;
+}
+
+/* 리스트 아이템 트랜지션 효과 */
+
+.list-enter-active,
+.list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(30px);
 }
 </style>
